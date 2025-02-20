0ed5cf8dc5555af9fa4fa39b27ebb297ed3e6022
jjuh00 <jamsen.juho@gmail.com>
2025-02-20 18:50:08 +0200

    Update Menu.tsx

--- country-search/src/components/Menu.tsx
+++ country-search/src/components/Menu.tsx
@@ -3,43 +3,43 @@ import SearchHistory from "./SearchHistory";
 import { SearchHistoryItem } from "../types/History";
 
 interface MenuProps {
-  onHistorySelected: (countryName: string) => void;
-  searchHistory: SearchHistoryItem[];
+    onHistorySelected: (countryName: string) => void;
+    searchHistory: SearchHistoryItem[];
 }
 
 const DropdownMenu: React.FC<MenuProps> = ({ onHistorySelected, searchHistory }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid">
-        <h1 className="mb-0">Country Search</h1>
-        <div className="dropdown ms-auto">
-          <button
-            className="btn dropdown-toggle"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <i className="fi fi-tr-bars-staggered"></i>
-          </button>
-          {isMenuOpen && (
-            <div className="dropdown-menu show">
-              <div className="dropdown-item d-flex justiy-content-between align-items-center" onClick={() => {}}>
-                History &emsp;&emsp;&emsp;&emsp;&emsp;
-                <i className="fi fi-ss-time-past ms-2"></i>
-              </div>
-              <SearchHistory
-                history={searchHistory}
-                onCountrySelected={(countryName) => {
-                  onHistorySelected(countryName);
-                  setIsMenuOpen(false);
-                }}
-              />
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container-fluid">
+                <h1 className="mb-0">Country Search</h1>
+                <div className="dropdown ms-auto">
+                    <button
+                        className="btn dropdown-toggle"
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        >
+                        <i className="fi fi-tr-bars-staggered"></i>
+                    </button>
+                    {isMenuOpen && (
+                        <div className="dropdown-menu show">
+                            <div className="dropdown-item d-flex justiy-content-between align-items-center" onClick={() => {}}>
+                                History &emsp;&emsp;&emsp;&emsp;&emsp;
+                                <i className="fi fi-ss-time-past ms-2"></i>
+                            </div>
+                            <SearchHistory
+                                history={searchHistory}
+                                onCountrySelected={(countryName) => {
+                                    onHistorySelected(countryName);
+                                    setIsMenuOpen(false);
+                                }}
+                            />
+                        </div>
+                    )}
+                </div>
             </div>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default DropdownMenu;
