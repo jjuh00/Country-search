9a2c391d4f3a92f223e30474085854e61c4c26f5
jjuh00 <jamsen.juho@gmail.com>
2025-02-20 15:02:42 +0200

    Update Api.tsx

--- country-search/src/components/Api.tsx
+++ country-search/src/components/Api.tsx
@@ -1,4 +1,4 @@
-// UseEffect isn't used since fetching is done on demand (when user clicks/presses enter)
+// UseEffect isn't used since fetching is done on demand (when user clicks button/presses enter)
 export const fetchCountry = async (countryName: string): Promise<Response> => {
   const formattedName = countryName.trim().replace(/\s+/g, "%20");
   const url = `https://restcountries.com/v3.1/name/${formattedName}`;
