aee7d829ffc08b23d6819046e2d275f79109b514
jjuh00 <jamsen.juho@gmail.com>
2025-02-20 17:57:52 +0200

    Add a hook

---
+++ country-search/src/hooks/UseHistory.ts
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { SearchHistoryItem } from "../types/History";
+
+const HISTORY_KEY = "countrySearchHistory"
+const MAX_ITEMS = 10;
+
+export const useSearchHistory = () => {
+    const [searchHistory, setSearchHistory] = useState<SearchHistoryItem[]>(() => {
+        const savedHistory = localStorage.getItem(HISTORY_KEY);
+        return savedHistory ? JSON.parse(savedHistory) : [];
+    });
+
+    useEffect (() => {
+        localStorage.setItem(HISTORY_KEY, JSON.stringify(searchHistory));
+    }, [searchHistory]);
+
+    const addToHistory = (countryName: string) => {
+        setSearchHistory(prevHistory => {
+            const newHistory = [
+                { countryName, time: Date.now() },
+                ...prevHistory.filter(item => item.countryName !== countryName)
+            ].slice(0, MAX_ITEMS);
+            return newHistory;
+        });
+    };
+
+    return { searchHistory, addToHistory };
+}
